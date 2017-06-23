5dc4a22063a396dadd68184e9565118dbae3c988
BioMaRu <Bio.Color.Palette@gmail.com>
2017-06-23 23:11:17 +0700

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Login from '@/containers/login'
+import Home from '@/containers/home'
+import Collection from '@/containers/collection'
+import Lobby from '@/containers/lobby'
+import Battle from '@/containers/battle'
+import Classes from '@/containers/classes'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/collection',
+      name: 'Collection',
+      component: Collection
+    },
+    {
+      path: '/lobby',
+      name: 'Lobby',
+      component: Lobby
+    },
+    {
+      path: '/classes',
+      name: 'Classes',
+      component: Classes
+    },
+    {
+      path: '/battle',
+      name: 'Battle',
+      component: Battle
     }
   ]
 })
