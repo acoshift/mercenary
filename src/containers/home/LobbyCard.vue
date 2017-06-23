4195cbd17315920225ea1e384320003923c307d9
BioMaRu <Bio.Color.Palette@gmail.com>
2017-06-24 00:37:05 +0700

    add homepage

---
+++ src/containers/home/LobbyCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="lunar-card _flex-row">
+    <img
+      src="~@/assets/enemy/enemy3-s.png"
+      width="100"
+      height="100"
+      class="enemy3">
+
+    <div class="lunar-segment">
+      <div class="lunar-block">Host: สมชาย</div>
+      <div class="lunar-block"></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LobbyCard'
+}
+</script>
+
+<style lang='scss' scoped>
+.enemy3 {
+  background: #dacab4;
+}
+</style>
--- src/containers/home/index.vue
+++ src/containers/home/index.vue
@@ -1,11 +1,105 @@
 <template>
   <div>
-    Home
+    <div class="_flex-column _full-height">
+
+      <div class="navbar _bg-color-main">
+        <div class="_full-height _flex-row _cross-center _main-space-between">
+          <div class="_font-size-big">Home</div>
+          <a href class="_color-accent">
+            <div class="_font-size-big">Collectible</div>
+          </a>
+        </div>
+      </div>
+
+      <div class="navbar-dummy">
+      </div>
+
+      <div class="lobby _flex-span _bg-color-base">
+        <div class="grid-container row">
+
+          <div class="col-xs-12">
+            <LobbyCard>
+            </LobbyCard>
+          </div>
+
+          <div class="col-xs-12">
+            <LobbyCard>
+            </LobbyCard>
+          </div>
+
+          <div class="col-xs-12">
+            <LobbyCard>
+            </LobbyCard>
+          </div>
+
+          <div class="col-xs-12">
+            <LobbyCard>
+            </LobbyCard>
+          </div>
+
+          <div class="col-xs-12">
+            <LobbyCard>
+            </LobbyCard>
+          </div>
+
+          <div class="col-xs-12">
+            <LobbyCard>
+            </LobbyCard>
+          </div>
+
+          <div class="col-xs-12">
+            <LobbyCard>
+            </LobbyCard>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="foot-button _bg-color-accent _color-light _flex-row _main-center _cross-center">
+        <h3 class="_no-margin">Create Room</h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import LobbyCard from './LobbyCard'
+export default {
+  name: 'Home',
+  components: {
+    LobbyCard
+  }
+}
 </script>
 
-<style>
+<style lang='scss' scoped>
+$navbar-height: 50px;
+
+.navbar {
+  position: fixed;
+  width: 100%;
+  color: white;
+  height: $navbar-height;
+  box-shadow: 0 15px 35px rgba(50, 50, 93, 0.03), 0 5px 15px rgba(0, 0, 0, 0.06);
+  > div {
+    padding: 0 16px;
+  }
+}
+
+.navbar-dummy {
+  height: $navbar-height;
+}
+
+.lobby {
+  overflow: auto;
+  padding-top: 16px;
+  background-color: black;
+  background-image: url('~@/assets/bg/bg1.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.foot-button {
+  height: 50px;
+}
 </style>
