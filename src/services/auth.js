2e8798fe107217fd16147b7defdfa1629715c8d2
acoshift <acoshift@gmail.com>
2017-06-24 00:17:17 +0700

    change user before add data

--- src/services/auth.js
+++ src/services/auth.js
@@ -9,24 +9,27 @@ const initUser = {}
 const $currentUser = new BehaviorSubject(initUser)
 firebase.auth().onAuthStateChanged((user) => {
   $currentUser.next(user)
-  // add user data if not exists
-  firebase.database().ref(`user/${user.uid}`).once('value', (snapshot) => {
-    const data = snapshot.val()
-    if (!data) {
-      // upload user image
-      axios.get(user.photoURL, { responseType: 'blob' }).then((res) => {
-        firebase.storage()
-          .ref(`/user/${user.uid}`)
-          .put(res.data, { cacheControl: 'public, max-age=7200' })
-          .then((f) => {
-            firebase.database().ref(`user/${user.uid}`).set({
-              name: user.displayName,
-              photo: f.downloadURL
+
+  if (user) {
+    // add user data if not exists
+    firebase.database().ref(`user/${user.uid}`).once('value', (snapshot) => {
+      const data = snapshot.val()
+      if (!data) {
+        // upload user image
+        axios.get(user.photoURL, { responseType: 'blob' }).then((res) => {
+          firebase.storage()
+            .ref(`/user/${user.uid}`)
+            .put(res.data, { cacheControl: 'public, max-age=7200' })
+            .then((f) => {
+              firebase.database().ref(`user/${user.uid}`).set({
+                name: user.displayName,
+                photo: f.downloadURL
+              })
             })
-          })
-      })
-    }
-  })
+        })
+      }
+    })
+  }
 })
 
 export const currentUser = $currentUser.asObservable().filter((x) => x !== initUser)
