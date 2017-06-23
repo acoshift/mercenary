22f00f4893e69f414052d4d5f338d9a75d5caf4c
acoshift <acoshift@gmail.com>
2017-06-23 22:57:52 +0700

    add firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB4182glIobO1V4xHx7tD1fWB1wvgxBqXo',
+  authDomain: 'mercenary-714ba.firebaseapp.com',
+  databaseURL: 'https://mercenary-714ba.firebaseio.com',
+  projectId: 'mercenary-714ba',
+  storageBucket: 'mercenary-714ba.appspot.com',
+  messagingSenderId: '665723839060'
+})
 
 /* eslint-disable no-new */
 new Vue({
