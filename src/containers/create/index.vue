e5fd3cba4832665eb50f1071d39bb295561f9553
acoshift <acoshift@gmail.com>
2017-06-24 20:18:53 +0700

    fix create room

--- src/containers/create/index.vue
+++ src/containers/create/index.vue
@@ -89,11 +89,9 @@ export default {
       }
 
       Room.create(this.selectedBoss, this.selectedJob)
-        .do(console.log)
         .subscribe(
-          (id) => {
-            console.log(id)
-            // this.$router.push({ name: 'Lobby'})
+          () => {
+            this.$router.push({ name: 'Lobby' })
           }
         )
     },
