0b597666313203b6691c17c1823263d2ab45ebbd
Chakri Lowphansirikul <artificialcc@gmail.com>
2017-06-25 15:22:26 +0700

    Add message when there is no room available

--- src/containers/home/index.vue
+++ src/containers/home/index.vue
@@ -24,11 +24,14 @@
 
       <div class="navbar-dummy">
       </div>
-
+      
       <div class="content _flex-span _bg-color-base">
         <div class="grid-container row" style="max-width: 500px">
           <div @click="clickRoom(r)" v-for="r in rooms" :key="r.$key" class="col-xs-12">
             <LobbyCard :host="r.host" :boss="r.boss" :member="r.memberCount"></LobbyCard>
+          </div>
+          <div v-if="rooms.length === 0" class="col-xs-12 no-room-message">
+            No Room
           </div>
         </div>
       </div>
@@ -120,5 +123,14 @@ $navbar-height: 50px;
 
 .foot-button {
   height: 50px;
+}
+
+.no-room-message {
+  position: relative;
+  top: 5vh;
+  color: white;
+  font-size: 1.25em;
+  text-align: center;
+  background-color: #6F6F6F;
 }
 </style>
